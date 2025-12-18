bf7413a3fc194b0c16df8fd7336823834a39748d
Odedequeen <christopherqueen095@gmail.com>
2025-12-18 12:13:54 +0100

    Fix missing svg path

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -3,7 +3,7 @@ import clockicon from "../../assets/Icons/clock.svg";
 import locationicon from "../../assets/Icons/location.svg";
 import usericon from "../../assets/Icons/head.svg";
 import linkicon from "../../assets/Icons/lindein.svg";
-import FacebookIcon from "../../assets/Icons/facebook.svg";
+import FacebookIcon from "../../assets/Icons/Facebook.svg";
 import TwitterIcon from "../../assets/Icons/twitter.svg";
 import kypeIcon from "../../assets/Icons/skype.svg";
 
@@ -22,14 +22,15 @@ export default function Topbar() {
         <div className="topbar-right">
           <img src={usericon} alt="User Icon" className="topbar-icon" />
           <span className="topbar-text">Login / Register</span>
-          <img src={FacebookIcon} alt="Facebook Icon" className="topbar-icon" />
+          <img
+            src={FacebookIcon}
+            alt=" Facebook Icon"
+            className="topbar-icon"
+          />
           <img src={TwitterIcon} alt="Twitter Icon" className="topbar-icon" />
-          <img src={kypeIcon} alt="skype Icon" className="topbar-icon"/>
+          <img src={kypeIcon} alt="skype Icon" className="topbar-icon" />
           <img src={linkicon} alt="Link Icon" className="topbar-icon" />
-   
-   
         </div>
-
       </div>
     </div>
   );
